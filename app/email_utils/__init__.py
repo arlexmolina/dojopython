172e9b8f84c8e3ec977e1c9a7b59024c16687598
Mauricio Molina <mauricio.molina@ideaslab.com.co>
2020-05-03 00:49:50 -0500

    send mail2

--- app/email_utils/__init__.py
+++ app/email_utils/__init__.py
@@ -1,17 +1,15 @@
 from flask import render_template
-from app import celery, logger
-from app import mail
 from flask_mail import Message
 
+from app import logger
+from app import mail
 
-@celery.task()
+#borrar
 def send_async_email(msg_dict):
     msg = Message()
     msg.__dict__.update(msg_dict)
     mail.send(msg)
 
-
-@celery.task
 def send_async_emails(msg_dicts):
     with mail.connect() as conn:
         for msg_dict in msg_dicts:
@@ -30,8 +28,8 @@ def send_email(to, subject, template, **kwargs):
 
 
 def message_to_dict(to, subject, template, **kwargs):
-    msg = Message(subject='[Alertas Contables Bancolombia] ' + subject,
-                  sender=('Ideas Lab', 'info@ideaslab.com.co'),
+    msg = Message(subject='[RESERVA] ' + subject,
+                  sender=('app empresariales', 'materiapps@gmail.com'),
                   recipients=to)
     msg.body = render_template('emails/' + template + '.txt', **kwargs)
     msg.html = render_template('emails/' + template + '.html', **kwargs)
