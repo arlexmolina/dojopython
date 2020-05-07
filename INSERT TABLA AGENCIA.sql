2e92ef2adc7975882db3504746a4457ef002da6d
Pedroridz <pedro.rios@udea.edu.co>
2020-05-06 23:12:22 -0500

    lineas de comando inserts tablas foto propiedad servicio ciudad y agencia

---
+++ INSERT TABLA AGENCIA.sql
@@ -0,0 +1,7 @@
+/*
+-- Query: SELECT * FROM innodb.AGENCIA
+LIMIT 0, 1000
+
+
+*/
+INSERT INTO `` (`idAgencia`,`logo`,`nombreAgencia`) VALUES ('1','https://cutt.ly/myjquBa','S_TEAM');
---
+++ INSERTS DE SERVICIOS, PROPIEDADES 19-36.sql
@@ -0,0 +1,61 @@
+/*
+-- Query: SELECT * FROM innodb.SERVICIOS
+LIMIT 0, 1000
+
+-- Date: 2020-05-06 22:05
+*/
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('19','2 cuartos y 1 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('19','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('19','Tv por cable');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('20','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('20','intenet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('20','Tv por cable');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('21','2 cuartos y 1 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('21','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('21','piscina');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('22','1 cuarto y 1 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('22','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('22','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('23','2 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('23','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('23','tv por cable');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('24','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('24','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('24','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('24','tv por cable');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('25','2 cuartos y 1 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('25','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('25','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('26','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('26','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('26','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('27','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('27','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('27','piscina');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('28','4 cuartos y 3 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('28','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('28','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('29','3 cuartos y 1 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('29','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('29','piscina');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('30','1 cuarto y 1 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('30','aire acondicionado');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('30','tv por cable');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('31','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('31','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('31','piscina');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('32','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('32','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('32','piscina');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('33','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('33','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('33','parking');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('34','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('34','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('34','piscina');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('35','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('35','piscina');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('35','tv por cable');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('36','3 cuartos y 2 wc');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('36','internet');
+INSERT INTO `` (`idPropiedad`,`nombreServicio`) VALUES ('36','tv por cable');
